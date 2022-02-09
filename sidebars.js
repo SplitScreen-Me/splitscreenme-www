2e4824d64129ce1a07bf490337fbf49239a10715
Talos910 <87913453+Talos910@users.noreply.github.com>
2022-02-09 01:13:14 -0600

    protoinput

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     'About': ['what-is-splitscreen-me', 'history'],
-    'Getting Started': ['installation', 'usage', 'faq'],
+    'Getting Started': ['installation', 'usage', 'faq', 'proto'],
     'Handlers': ['handlers', 'create-handlers', 'kbm-setup'],
     'Games': ['games'],
   },
