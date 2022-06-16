d1aaa9fdf4aa586d75dd58c0b417eb0bc1376102
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2022-06-17 01:24:27 +0200

    Add API documentation. fix #76

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     'About': ['what-is-splitscreen-me', 'history'],
     'Getting Started': ['installation', 'usage', 'faq', 'proto'],
-    'Handlers': ['handlers', 'create-handlers', 'kbm-setup'],
+    'Handlers': ['handlers', 'create-handlers', 'kbm-setup', 'handler-api'],
     'Games': ['games'],
   },
 };
